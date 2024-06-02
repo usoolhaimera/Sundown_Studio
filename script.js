6f9f0dd9f037d9d409d7ed717575d7ee5e7af0fd
Ravi Verma <111685794+usoolhaimera@users.noreply.github.com>
2024-06-02 22:37:05 +0530

    changes

---
+++ script.js
@@ -0,0 +1,34 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true,
+});
+
+var elemC = document.querySelector(".elem-container");
+var fixed_img = document.querySelector(".fixed-image");
+elemC.addEventListener("mouseenter", function () {
+  fixed_img.style.display = "block";
+});
+elemC.addEventListener("mouseleave", function () {
+  fixed_img.style.display = "none";
+});
+
+var elems = document.querySelectorAll(".elem");
+
+elems.forEach(function (e) {
+  e.addEventListener("mouseenter", function () {
+    var image = e.getAttribute("data-image");
+    fixed_img.style.backgroundImage = `url(${image})`;
+  });
+});
+
+/****************swiper js *********** */
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
